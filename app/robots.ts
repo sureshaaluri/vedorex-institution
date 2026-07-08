b7a7c0b1071f80a1185fe9daa05fe2f6331c82f9
Deepthi Golusula <golusuladeepthi@gmail.com>
2026-07-08 10:55:32 +0530

    robots

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://academy.vedorex.com/sitemap.xml',
+  }
+}
